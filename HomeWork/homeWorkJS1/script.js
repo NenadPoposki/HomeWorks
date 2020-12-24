89c81a3e604794e2038efa2e52ab0dc5d61e3571
NenadPoposki <67703135+NenadPoposki@users.noreply.github.com>
2020-12-25 00:05:26 +0100

    Add files via upload

---
+++ HomeWork/homeWorkJS1/script.js
@@ -0,0 +1,38 @@
+
+//String
+var str = "Hello HomeWork";
+console.log(str);
+console.log(typeof(str));
+
+//Number
+var num = 1;
+console.log(num);
+console.log(typeof(num));
+
+//Boolean
+var bool = true;
+console.log(bool);
+console.log(typeof(bool));
+
+//Undefined
+var homeWork;
+console.log(homeWork);
+console.log(typeof(homeWork));
+
+//Null object
+var n = null;
+console.log(n);
+console.log(typeof(n));
+
+//Area of circle
+var r = prompt("insert value of radius: " );
+const p = 3.14;
+var area = r*(2*p);
+alert("The area of circle is: " + area);
+
+//% sell the phones
+var num = prompt("how many phones you want to buy?" );
+const pho = 119.95;
+const ta = 5;
+var total = num*(pho%ta);
+alert("The price will be:" + total);
