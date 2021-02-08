c9c62ade609d55be2abc5fe7345d0f41aab0c321
NenadPoposki <67703135+NenadPoposki@users.noreply.github.com>
2021-02-08 20:35:25 +0100

    Update script.js

--- HomeWork/exercises/exercise 1/script.js
+++ HomeWork/exercises/exercise 1/script.js
@@ -8,7 +8,7 @@
                 return false;
             }
         }
-        for(let i = 1; i <= arr.length; i++){
+        for(let i = 0; i <= arr.length-1; i++){
             if (arr[i+1]%2==0 && arr[i]%2==0) {
                 result.push(`-${arr[i]}`)
             }
